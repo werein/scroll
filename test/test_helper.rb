f09a71070a4dc1c725dcbe65b480caf813078383
Jiri Kolarik <jiri.kolarik@imin.cz>
2014-03-30 10:42:36 +0200

    New test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,43 @@
+# Keep it on top, don't change positions
+require 'codeclimate-test-reporter'
 require 'simplecov'
+require 'coveralls'
+
+# Don't forget set token
+CodeClimate::TestReporter.start
+
+Coveralls.wear! if Coveralls.should_run?
+# Coveralls.wear!('rails') # For RailsApp
 
 # Configure Rails Environment
-ENV['RAILS_ENV'] = 'test'
+ENV["RAILS_ENV"] = "test"
 
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
-require 'rails/test_help'
-require 'testable'
-require 'capybara/rails'
-include Testable::Helpers
-
-# Load factories
-Dir["#{File.dirname(__FILE__)}/factories/*.rb"].each { |f| require f }
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require "rails/test_help"
+require 'tuberack'
 
 Rails.backtrace_cleaner.remove_silencers!
 
-class ActiveSupport::TestCase
-  include Scroll::Engine.routes.url_helpers
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+# Load fixtures from the engine
+if ActiveSupport::TestCase.method_defined?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
+
+require 'capybara/rails' # Capybara must be required directly          
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
 end
+
+include Tuberack::Helpers
+
+class ActiveSupport::TestCase
+  # Uncomment if are you using Rails engine
+  include Scroll::Engine.routes.url_helpers
+end
+
+
+WebMock.disable_net_connect! allow: %w(coveralls.io)
